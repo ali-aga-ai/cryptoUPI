0ac165aea25af8a9a0fbd2b083e13dd77d889013
Kamaal2026 <f20220109@hyderabad.bits-pilani.ac.in>
2025-03-24 21:54:20 +0530

    added pwd as another field in data json or else the bank was crashing

--- user.js
+++ user.js
@@ -56,6 +56,7 @@ const connectToBankUser = () => {
     ifsc = await askQuestion("enter ifsc: ");
     phoneNum = await askQuestion("enter phone number: ");
     pin = await askQuestion("enter pin: ");
+    pwd = await askQuestion("enter password: ");
     // balance = await askQuestion("enter balance: ");
     // Checking whether balance is valid or not at user side
     while(true){
@@ -77,6 +78,7 @@ const connectToBankUser = () => {
         phoneNum: phoneNum,
         pin: pin,
         balance: balance,
+        pwd: pwd
       })
     );
   })();
