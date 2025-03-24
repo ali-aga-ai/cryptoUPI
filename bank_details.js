b86edc038c004558baf34a26453b6ca1618a0112
Mohammed Kamaalullah Khan Quadri <144052291+Kamaal2026@users.noreply.github.com>
2025-03-24 18:11:59 +0530

    added ifsc codes for all banks

--- bank_details.js
+++ bank_details.js
@@ -1,6 +1,6 @@
 const banks = { // the bank name is the key and the value is an array of ifsc codes
-    HDFC: ["hdfc1", "hdfc2"],
-    SBI: ["sbi1", "sbi2", "sbi3"],
-    ICICI: ["icici1", "icici2", "icici3"]
+    HDFC: ["hdfc1", "hdfc2","hdfc3"],
+    SBI: ["sbi1","sbi2","sbi3"],
+    ICICI: ["icici1","icici2","icici3"]
 };
 exports.banks = banks;
