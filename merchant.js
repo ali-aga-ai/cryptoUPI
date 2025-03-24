ac0fcbf4ebfcc9f49aea6a65915fd4fc5cbb16f0
Kamaal2026 <f20220109@hyderabad.bits-pilani.ac.in>
2025-03-24 21:53:03 +0530

    made the merchant take input from cli and output acc creation on cli

--- merchant.js
+++ merchant.js
@@ -1,28 +1,89 @@
+const readline = require("readline");
 const WebSocket = require("ws");
 
 
 const connectToBankMerchant = () =>{
-    const socket = new WebSocket("ws://localhost:8080"); // finds the socket of the bank to connect to
+  const socket = new WebSocket("ws://localhost:8080"); // finds the socket of the bank to connect to
 
-    socket.onopen = () => {
-        // on open, it sends a message to the server
-        socket.send(
-          JSON.stringify({
-            type: "init",
-            userType: "merchant",
-            userName: "merchant1",
-            bankName: "HDFC",
-            pwd: "password123",
-            ifsc: "hdfc1",
-            balance: 10000,
-          })
-        );
-        console.log("Connected to server");
-      };
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const askQuestion = (query) => {
+    return new Promise((resolve) => rl.question(query, resolve));
+  };
+  
+  (async () => {
+    mer_name = await askQuestion("enter name: ");
+    bankName = await askQuestion("enter bank name: ");//this wasn't mentioned in the assignment.
+    ifsc = await askQuestion("enter ifsc: ");
+    pwd = await askQuestion("enter password: ");
+    // balance = await askQuestion("enter balance: ");
+    // Checking whether balance is valid or not at merchant side
+    while(true){
+      balance = await askQuestion("enter balance: ");
+      if(isNaN(parseFloat(balance)) || parseFloat(balance) < 0){
+        console.log("Invalid balance. Please enter a valid balance.");
+      }
+      else{
+        break;
+      }
+    }
+
+    socket.send(
+      JSON.stringify({
+        type: "init",
+        userType: "merchant",
+        userName: mer_name,
+        bankName: bankName,
+        ifsc: ifsc,
+        balance: balance,
+        pwd: pwd
+      })
+    );
+  })();
+  
+
+  socket.onmessage = function(event) {
+    const response = JSON.parse(event.data);
+    
+    if (response.type === "error") {
+      if (response.errorType === "invalidBank") {
+        console.log("INVALID BANK NAME");
+        // connecting again to enter correct details
+        connectToBankUser();
+      } else if (response.errorType === "invalidIFSC") {
+        console.log("INVALID IFSC CODE");
+        // connecting again to enter correct details
+        connectToBankUser();
+      }
+    } else if (response.type === "success") {
+      console.log("Account created successfully. MID:", response.merchantID);
+      // connectToBankUser();
+    }
+  };
+
+    // socket.onopen = () => {
+    //     // on open, it sends a message to the server
+    //     socket.send(
+    //       JSON.stringify({
+    //         type: "init",
+    //         userType: "merchant",
+    //         userName: "merchant1",
+    //         bankName: "HDFC",
+    //         pwd: "password123",
+    //         ifsc: "hdfc1",
+    //         balance: 10000,
+    //       })
+    //     );
+    //     console.log("Connected to server");
+    //   };
       
-      socket.onmessage = (event) => {
-        console.log("Message from server:", event.data); // if it receives a message from the server, it logs it
-      };
+    //   socket.onmessage = (event) => {
+    //     console.log("Message from server:", event.data); // if it receives a message from the server, it logs it
+    //   };
+
 
 }
 
@@ -35,7 +96,7 @@ const connectToMachineMerchant = () =>{
           JSON.stringify({
             type: "init",
             userType: "merchant", 
-            merchantID: 4214214,
+            merchantID: 4214214,// will have to update with the merchant id generated.
           })
         );
       };
